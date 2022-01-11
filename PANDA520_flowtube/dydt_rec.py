670aed68aaef1df3da1e4959e677888bc1bd4d45
Jialishen <66317261+Jialishen@users.noreply.github.com>
2022-01-11 16:15:29 +0200

    small

--- PANDA520_flowtube/dydt_rec.py
+++ PANDA520_flowtube/dydt_rec.py
@@ -2,7 +2,7 @@
 # changes due to gas-phase photochemistry and partitioning are included; 
 # generated in init_conc and treats loss from gas-phase as negative
 
-# File Created at 2021-12-21 14:23:55.317246
+# File Created at 2021-12-21 14:39:38.769957
 
 import numpy as np 
 
