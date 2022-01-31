4faddf59985af1057afa6aca6ddeeb4f9b10a9db
Jialishen <66317261+Jialishen@users.noreply.github.com>
2022-01-31 12:09:38 +0200

    bug

--- PANDA520_flowtube/Input_Parameters_SA_cali.py
+++ PANDA520_flowtube/Input_Parameters_SA_cali.py
@@ -6,13 +6,12 @@ for name in dir():
         del globals()[name]
 del name
 
-#%% import functions 
+#% import functions 
 import numpy as np
 import pandas as pd
 from Vapour_calc import H2O_conc as H2O_conc
 from cmd_calib5 import cmd_calib5
 import os
-
 
 class UnitFloat(float):
 
@@ -29,12 +28,12 @@ class UnitFloat(float):
 1 inch inner diamerters is 0.78/4*3
 is 2.54 cm, 0.2 for the tube wall
 '''
-#%%Prepare the inputs
+#%% Prepare the inputs
   
 # load H2O Q  
-file = os.getcwd() + '/input_files/H2O_3.csv'
+file = os.getcwd() + '/input_files/H2O_2.csv'
 
-H2O_data=pd.read_csv(file)
+H2O_data = pd.read_csv(file)
 
 ''' set temperature and press '''
 # T_cel = 25  # C
@@ -87,7 +86,6 @@ outflowLocation = 'before' # outflow tube located before or after injecting air,
 
 fullOrSimpleModel = 'full' # simple: Gormley&Kennedy approximation, full: flow model (much slower)
 
-
 # calculate the conc for const species here, this file is SO2, O2, H2O
 
 WaterFlow1 = H2O_data['H2O_1']
@@ -102,7 +100,6 @@ O2conc1 = O2inAir * AirFlow / 1000 / totFlow * p / 1.3806488e-23 / T / 1e6
 H2Oconc1 = WaterFlow1 / 1000 / totFlow * H2O_conc(T_cel, 1).SatP[0] / 1.3806488e-23 / T / 1e6
 SO2conc1 = SO2Flow / 1000 / totFlow * SO2BottlePpm * 1e-6 * p / 1.3806488e-23 / T / 1e6
 
-
 WaterFlow2 = H2O_data['H2O_2']   # second H2O flow 
 totFlow2 = Q2 * np.ones(WaterFlow1.shape)
 H2Oconc2 = (WaterFlow2 + WaterFlow1) / 1000 / totFlow2 * H2O_conc(T_cel, 1).SatP[0] / 1.3806488e-23 / T / 1e6
@@ -110,9 +107,9 @@ O2conc2 =  O2conc1 * Q1/Q2
 SO2conc2 = SO2conc1* Q1/Q2
 
 if flag_tube == '1':
-    H2Oconc2 = 0
-    O2conc2 = 0
-    SO2conc2 = 0
+    H2Oconc2 = pd.Series(np.zeros(WaterFlow1.shape))
+    O2conc2 = pd.Series(np.zeros(WaterFlow1.shape))
+    SO2conc2 = pd.Series(np.zeros(WaterFlow1.shape))
 
 H2Oconc = np.transpose([H2Oconc1,H2Oconc2])
 O2conc =  np.transpose([O2conc1,O2conc2])
@@ -202,9 +199,11 @@ params = {'T' : UnitFloat(T, "K"), # temperaure
 #% 
 for i in range(8):
     print(list(params.keys())[i], list(params.values())[i], list(params.values())[i].unit)
-# i =1 
+
+# i = 6
 # const_comp_conc= const_comp_conc[:,i,:]
 # Init_comp_conc=Init_comp_conc[i]
+
 #%% computation begins
 meanconc = []
 
@@ -216,6 +215,7 @@ for i in range(WaterFlow1.size):#range(H2SO4.size):
         meanconc.append(meanConc1) 
         c.append(c1)
 
+# meanconc = meanConc1
 meanconc_s = pd.DataFrame(np.transpose(meanconc)) 
 meanconc_s.index = plot_spec
     
@@ -223,9 +223,9 @@ meanconc_s.index = plot_spec
 
 meanconc_s = pd.DataFrame(np.transpose(meanconc)) 
 meanconc_s.index = plot_spec
-meanconc_s.to_csv('C:/Users/jiali/MION2-AMT-paper/MION2-AMT-paper/script/SA_cali/input_files/SA_model_3_mean.csv')
+meanconc_s.to_csv('C:/Users/jiali/MION2-AMT-paper/MION2-AMT-paper/script/SA_cali/input_files/SA_model_2_mean.csv')
 
-with open('C:/Users/jiali/MION2-AMT-paper/MION2-AMT-paper/script/SA_cali/input_files/SA_model_3_c.txt', 'w') as f:
+with open('C:/Users/jiali/MION2-AMT-paper/MION2-AMT-paper/script/SA_cali/input_files/SA_model_2_c.txt', 'w') as f:
     for item in c:
         f.write("%s\n" % item)
 
--- PANDA520_flowtube/dydt_rec.py
+++ PANDA520_flowtube/dydt_rec.py
@@ -2,7 +2,7 @@
 # changes due to gas-phase photochemistry and partitioning are included; 
 # generated in init_conc and treats loss from gas-phase as negative
 
-# File Created at 2022-01-27 10:00:55.896983
+# File Created at 2022-01-31 12:00:59.198403
 
 import numpy as np 
 
