48740f96218d44a5483dfafd9f8a9695dbf96ba0
jialishen <jiali.shen@helsinki.fi>
2022-12-22 10:52:59 +0200

    rate-coeffs

--- PANDA520_flowtube/dydt_rec.py
+++ PANDA520_flowtube/dydt_rec.py
@@ -2,7 +2,7 @@
 # changes due to gas-phase photochemistry and partitioning are included; 
 # generated in init_conc and treats loss from gas-phase as negative
 
-# File Created at 2022-12-22 10:51:35.877890
+# File Created at 2022-12-22 10:52:34.416278
 
 import numpy as np 
 
--- PANDA520_flowtube/rate_coeffs.py
+++ PANDA520_flowtube/rate_coeffs.py
@@ -1,6 +1,6 @@
 '''module for calculating reaction rate coefficients (automatically generated)'''
 # module to hold expressions for calculating rate coefficients # 
-# created at 2022-12-22 10:51:35.875624
+# created at 2022-12-22 10:52:34.414105
 
 import numpy
 
