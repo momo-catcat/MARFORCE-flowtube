ee7c562eb42490d76bc8e2e742a5f9644944b8fa
momo-catcat <xuchenghe93@gmail.com>
2022-12-22 10:35:55 +0200

    Regular update

--- PANDA520_flowtube/rate_coeffs.py
+++ PANDA520_flowtube/rate_coeffs.py
@@ -1,6 +1,6 @@
 '''module for calculating reaction rate coefficients (automatically generated)'''
 # module to hold expressions for calculating rate coefficients # 
-# created at 2022-12-21 17:18:32.926937
+# created at 2022-12-22 10:34:02.942639
 
 import numpy
 
