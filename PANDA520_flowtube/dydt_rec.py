0b0eb2b9214a9a41dc95a0d39b7898ac74f76003
momo-catcat <xuchenghe93@gmail.com>
2022-12-22 10:43:53 +0200

    Regular update

--- PANDA520_flowtube/dydt_rec.py
+++ PANDA520_flowtube/dydt_rec.py
@@ -2,7 +2,7 @@
 # changes due to gas-phase photochemistry and partitioning are included; 
 # generated in init_conc and treats loss from gas-phase as negative
 
-# File Created at 2022-12-22 09:58:57.378978
+# File Created at 2022-12-22 10:43:31.464818
 
 import numpy as np 
 
