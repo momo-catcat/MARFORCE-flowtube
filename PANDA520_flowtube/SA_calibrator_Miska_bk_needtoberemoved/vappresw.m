656f59e758bd08ea792c60bd10cc4252fbb14463
jialishen <jiali.shen@helsinki.fi>
2023-01-04 15:57:01 +0200

    delete

--- PANDA520_flowtube/SA_calibrator_Miska_bk_needtoberemoved/vappresw.m
+++
@@ -1,8 +0,0 @@
-function [ tulos ] = vappresw( T )
-%veden saturaatiohöyrynpaine (Pa) lämpötilassa T (K)
-%Preining 1981
-
-tulos = exp(77.34491296-7235.424651./T-8.2.*log(T)+0.0057113.*T);
-
-end
-
